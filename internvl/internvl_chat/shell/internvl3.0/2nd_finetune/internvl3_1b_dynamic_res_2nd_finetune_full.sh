7729ea80786c47c7bf60ae9ad642486f88eb40e5
OmerAJ <23100050@lums.edu.pk>
2025-07-30 13:01:15 +0500

    use bf16

--- internvl/internvl_chat/shell/internvl3.0/2nd_finetune/internvl3_1b_dynamic_res_2nd_finetune_full.sh
+++ internvl/internvl_chat/shell/internvl3.0/2nd_finetune/internvl3_1b_dynamic_res_2nd_finetune_full.sh
@@ -44,7 +44,7 @@ torchrun \
   --freeze_backbone True \
   --vision_select_layer -1 \
   --dataloader_num_workers 4 \
-  --bf16 False \
+  --bf16 True \
   --num_train_epochs 1 \
   --per_device_train_batch_size ${PER_DEVICE_BATCH_SIZE} \
   --gradient_accumulation_steps ${GRADIENT_ACC} \
