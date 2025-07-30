f69ee352db55345e2b973cbfcd7e16768018a9c7
OmerAJ <23100050@lums.edu.pk>
2025-07-30 13:53:01 +0500

    .

--- internvl/internvl_chat/internvl/patch/__init__.py
+++ internvl/internvl_chat/internvl/patch/__init__.py
@@ -14,8 +14,8 @@ from .llama_rmsnorm_monkey_patch import \
 from .pad_data_collator import (concat_pad_data_collator,
                                 dpo_concat_pad_data_collator,
                                 pad_data_collator)
-from .phi3_packed_training_patch import replace_phi3_attention_class
-from .qwen2_packed_training_patch import replace_qwen2_attention_class
+# from .phi3_packed_training_patch import replace_phi3_attention_class
+# from .qwen2_packed_training_patch import replace_qwen2_attention_class
 from .train_dataloader_patch import replace_train_dataloader
 from .train_sampler_patch import replace_train_sampler
 
@@ -25,8 +25,8 @@ __all__ = ['replace_llama_attn_with_flash_attn',
            'replace_train_sampler',
            'replace_train_dataloader',
            'replace_internlm2_attention_class',
-           'replace_qwen2_attention_class',
-           'replace_phi3_attention_class',
+        #    'replace_qwen2_attention_class',
+        #    'replace_phi3_attention_class',
            'replace_llama_attention_class',
            'pad_data_collator',
            'dpo_concat_pad_data_collator',
