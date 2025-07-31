07b8092bff7c3d0aa9aa2d272a2fe0fa9d1e28a7
OmerAJ <23100050@lums.edu.pk>
2025-07-31 13:34:59 +0500

    .

--- internvl/internvl_chat/shell/internvl3.0/2nd_finetune/internvl3_2b_dynamic_res_2nd_finetune_full.sh
+++ internvl/internvl_chat/shell/internvl3.0/2nd_finetune/internvl3_2b_dynamic_res_2nd_finetune_full.sh
@@ -48,7 +48,6 @@ torchrun \
   --num_train_epochs 20 \
   --per_device_train_batch_size ${PER_DEVICE_BATCH_SIZE} \
   --gradient_accumulation_steps ${GRADIENT_ACC} \
-  --evaluation_strategy "no" \
   --save_strategy "steps" \
   --save_steps 200 \
   --save_total_limit 1 \
