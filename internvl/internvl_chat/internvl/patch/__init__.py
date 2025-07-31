21cf15bbb5d5ba88668ece74e980d3528feb04e7
OmerAJ <23100050@lums.edu.pk>
2025-07-31 12:31:10 +0500

    remove llama patch

--- internvl/internvl_chat/internvl/patch/__init__.py
+++ internvl/internvl_chat/internvl/patch/__init__.py
@@ -8,7 +8,7 @@ from .internlm2_packed_training_patch import replace_internlm2_attention_class
 from .internvit_liger_monkey_patch import apply_liger_kernel_to_internvit
 from .llama2_flash_attn_monkey_patch import replace_llama2_attn_with_flash_attn
 from .llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
-from .llama_packed_training_patch import replace_llama_attention_class
+# from .llama_packed_training_patch import replace_llama_attention_class
 from .llama_rmsnorm_monkey_patch import \
     replace_llama_rmsnorm_with_fused_rmsnorm
 from .pad_data_collator import (concat_pad_data_collator,
@@ -27,7 +27,7 @@ __all__ = ['replace_llama_attn_with_flash_attn',
            'replace_internlm2_attention_class',
         #    'replace_qwen2_attention_class',
         #    'replace_phi3_attention_class',
-           'replace_llama_attention_class',
+        #    'replace_llama_attention_class',
            'pad_data_collator',
            'dpo_concat_pad_data_collator',
            'concat_pad_data_collator',
